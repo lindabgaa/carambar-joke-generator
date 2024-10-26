3f167349e75beaafe64d9a2714f591d3a1612442
Linda BOUGAA <lindabgaa@Host-003.lan>
2024-10-26 18:07:43 +0200

    code app component + css styles

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# React + Vite
+# Carambar & Co Landing Page - Projet React
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+![Project Status](https://img.shields.io/badge/Project%20Status-In%20Progress-orange)
 
-Currently, two official plugins are available:
+## Technologies
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- **Frontend**: React, Vite, Axios, CSS
+- **API**:
+
+## Installation & Configuration
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
+import axios from "axios";
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return;
+  const [joke, setJoke] = useState(null);
+
+  const handleButtonClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://blagues-carambar-api.onrender.com/api/v1/blagues/random"
+      );
+
+      const jokeData = response.data;
+
+      const { question, answer } = jokeData;
+
+      if (!question || !answer) {
+        return;
+      }
+
+      setJoke({ question, answer });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <main>
+      <header>
+        <h1>Carambar & Co </h1>
+      </header>
+
+      <section>
+        <p className="intro">Un clic, une blague, un sourire !</p>
+
+        <div className="joke-generator">
+          <button
+            type="button"
+            onClick={handleButtonClick}
+            aria-label="Obtenir une blague"
+          >
+            {!joke ? "Clique moi !" : "Encore !"}
+          </button>
+          {joke && (
+            <div className="joke-display">
+              <p className="joke-question">{joke.question}</p>
+              <p className="joke-answer">{joke.answer}</p>
+            </div>
+          )}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
