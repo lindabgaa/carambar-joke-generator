a3abfbb70343acf745cfe10b0e99a4e3bab08795
Linda BOUGAA <lindabgaa@Host-003.lan>
2024-10-30 16:18:23 +0100

    update css styles #1

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import "./App.css";
 
 function App() {
   const [joke, setJoke] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(
+    "Oups, impossible de récupérer une blague pour le moment. Réessaye plus tard !"
+  );
 
   const handleButtonClick = async () => {
+    setErrorMessage(null);
+
     try {
       const response = await axios.get(
         "https://carambar-jokes-api.onrender.com/api/v1/jokes/random"
@@ -16,13 +21,26 @@ function App() {
 
       const { question, answer } = jokeData;
 
-      if (!question || !answer) {
-        return;
+      if (!question && !answer) {
+        setJoke(null);
+        throw new Error(
+          "Unable to retrieve a joke: both question and answer are missing."
+        );
+      } else if (!question) {
+        setJoke(null);
+        throw new Error("Unable to retrieve a joke: question is missing.");
+      } else if (!answer) {
+        setJoke(null);
+        throw new Error("Unable to retrieve a joke: answer is missing.");
       }
 
       setJoke({ question, answer });
     } catch (error) {
       console.log(error);
+      setJoke(null);
+      setErrorMessage(
+        "Oups, impossible de récupérer une blague pour le moment. Réessaye plus tard !"
+      );
     }
   };
 
@@ -43,12 +61,15 @@ function App() {
           >
             {!joke ? "Clique moi !" : "Encore !"}
           </button>
-          {joke && (
+
+          {errorMessage ? (
+            <p className="error-message">{errorMessage}</p>
+          ) : joke ? (
             <div className="joke-display">
               <p className="joke-question">{joke.question}</p>
               <p className="joke-answer">{joke.answer}</p>
             </div>
-          )}
+          ) : null}
         </div>
       </section>
     </main>
